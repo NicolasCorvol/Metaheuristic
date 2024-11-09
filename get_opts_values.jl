4c67fbfc11e7f5129bf603d4cee0d250bf6b8f25
NicolasCorvol <nicolas.corvol@gmail.com>
2024-11-09 19:22:16 +0100

    create get opt min

--- get_opts_values.jl
+++ get_opts_values.jl
@@ -1,9 +1,22 @@
-function getOpts()::Dict{String,Int32}
+function getOptsgap_max()::Dict{String,Int32}
     opts = Dict{String,Int32}()
     all_opt = readdlm("instances/opt.txt")
     for i in 1:12
         for j in 1:5
             opts["gap$(i)_$(j)"] = all_opt[i, j]
+        end
+    end
+    return opts
+end
+
+function getOpts_min()::Dict{String,Int32}
+    opts = Dict{String,Int32}()
+    names = ["a", "b", "c", "d"]
+    all_opt = readdlm("instances/opt_min.txt")
+    for i in 1:5
+        name = names[i]
+        for j in 1:6
+            opts["gap$(name)_$(j)"] = all_opt[i, j]
         end
     end
     return opts
--- main.jl
+++ main.jl
@@ -16,7 +16,8 @@ function main()
     println(Threads.nthreads())
     my_lock = ReentrantLock()
     opts = Dict{String,Int32}()
-    all_opts = getOpts()
+    all_opts = getOptsgap_max()
+    all_opt = getOpts_min()
     df = DataFrame(Instance = ["gap$(i)_$(j+1)" for i in 1:12 for j in 0:4],
                    Opts = [all_opts["gap$(i)_$(j+1)"] for i in 1:12 for j in 0:4])
     best_initial_cost = []
